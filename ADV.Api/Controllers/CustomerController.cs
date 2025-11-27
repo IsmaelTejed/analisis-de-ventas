9bad1a8d8127819757046cfe049f9f968fa2c8d9
IsmaelTejed <ismael261126@gmail.com>
2025-11-27 17:09:23 -0400

    cambios en la api

---
+++ ADV.Api/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using ADV.Application.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADV.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customers = new List<CustomerDto>
+            {
+       
+                new CustomerDto { Id = 501, Name = "Juan Pérez", Email = "juan@example.com", Country = "España" },
+                new CustomerDto { Id = 502, Name = "Maria Gonzales", Email = "maria@example.com", Country = "Argentina" },
+                new CustomerDto { Id = 503, Name = "Luisa Miller", Email = "luisa@example.com", Country = "Estados Unidos" }
+             };
+
+                 return Ok(customers);
+        }
+    }
+}
---
+++ ADV.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ADV.Application.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADV.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = new List<ProductDto>
+            {
+                new ProductDto { Id = 201, Title = "Smartphone Ultra X", Price = 899.99m, Category = "Mobile" },
+                new ProductDto { Id = 202, Title = "Audífonos Bluetooth Pro", Price = 59.90m, Category = "Audio" },
+                new ProductDto { Id = 203, Title = "Silla Gamer Ergonomic", Price = 199.50m, Category = "Furniture" },
+                new ProductDto { Id = 204, Title = "Tablet Lite 10''", Price = 329.00m, Category = "Mobile" }
+            };
+
+            return Ok(products);
+        }
+
+    }
+}
